b2151a0a9174c37f7e60004dc4c37ab9dd22babe
jennmez <jennmez@gmail.com>
2020-10-26 12:15:17 -0500

    css complete for now

--- src/Card.js
+++ src/Card.js
@@ -4,12 +4,27 @@ import './Card.css';
 class Card extends React.Component {
   render() {
     const profile = this.props;
+    console.log(profile);
     return (
       <div className="github-profile">
-        <img src={profile.avatar_url} />
+        {/* <div className="img-resize"> */}
+        <img className="img-resize" src={profile.avatar_url} />
+        {/* </div> */}
         <div className="info">
-          <div className="name">{profile.name}</div>
-          <div className="company">{profile.company}</div>
+          <div className="login">{profile.login}</div>
+          {profile.name ? (
+            <div className="name">{profile.name}</div>
+          ) : (
+            <div className="name">Name: 🤷‍♀️</div>
+          )}
+          {profile.location ? (
+            <div className="location">{profile.location}</div>
+          ) : (
+            <div className="location">Location: 🤷‍♀️</div>
+          )}
+          <div className="public-repos">
+            Public Repos: {profile.public_repos}
+          </div>
         </div>
       </div>
     );
